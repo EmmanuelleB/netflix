f1964f5723d734e6d317584bbca8f1a55f11f400
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-06-30 19:32:56 +0200

    add font

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Section from "./components/section/Section";
 
 function App() {
   return (
-    <div className="container">   
+    <div >   
       <Header />
       
       {moviesList.map((item,index)=>{
