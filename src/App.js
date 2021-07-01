fc56be7ecd305d964da44d27765f63b28f6b85a1
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-07-01 11:19:50 +0200

    add function for  alt img

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   return (
     <div >   
       <Header />
+      {/* <Section category={moviesList[0].category} images={moviesList[0].images} />
+      <Section category={moviesList[1].category} images={moviesList[1].images} />
+      <Section category={moviesList[2].category} images={moviesList[2].images} />
+      <Section category={moviesList[3].category} images={moviesList[3].images} /> */}
       
       {moviesList.map((item,index)=>{
         return (
--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -2,14 +2,24 @@ import "./Section.css"
 import moviesList from "./../../moviesList.json"
 
 const Section = ({category, images})=>{
+    
+    const altMovies = (str)=>{
+        let array = str.split("/");
+        let altStr = array[array.length-1];
+        return altStr;
+    }
+
     return(
         <div className="container-section">
         <h1>{category}</h1>
         <div>
+        {/* <img src={moviesList[0].images[0]} alt="movies"/>
+        <img src={moviesList[0].images[1]} alt="movies"/>
+        <img src={moviesList[0].images[2]} alt="movies"/> */}
 
         {images.map((image, index) => {
            return(
-                <img key={index} src={image} alt="movies" />
+                <img key={index} src={image} alt={altMovies(image)} />
                 );
         })};
 
