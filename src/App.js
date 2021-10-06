8828724952c2a4d6c6cc120c5e461a2a42b33945
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-10-06 12:08:15 +0200

    hide scrollbar on all navigators

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import "./App.css";
-import moviesList from "./moviesList.json"
+import moviesList from "./moviesList.json";
 import Header from "./components/header/Header";
 import Section from "./components/section/Section";
 
-
 function App() {
   return (
-    <div >   
+    <div>
       <Header />
-      {/* <Section category={moviesList[0].category} images={moviesList[0].images} />
-      <Section category={moviesList[1].category} images={moviesList[1].images} />
-      <Section category={moviesList[2].category} images={moviesList[2].images} />
-      <Section category={moviesList[3].category} images={moviesList[3].images} /> */}
-      
-      {moviesList.map((item,index)=>{
-        return (
-          <Section key={index} category={item.category} images={item.images} />
-        )
-        
+
+      {moviesList.map((item, index) => {
+        return <Section key={index} category={item.category} images={item.images} />;
       })}
-      
     </div>
   );
 }
--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -1,33 +1,24 @@
-import "./Section.css"
-import moviesList from "./../../moviesList.json"
+import "./Section.css";
+import moviesList from "./../../moviesList.json";
 
-const Section = ({category, images})=>{
-    
-    const altMovies = (str)=>{
-        let array = str.split("/");
-        let altStr = array[array.length-1];
-        return altStr;
-    }
+const Section = ({ category, images }) => {
+  const altMovies = (str) => {
+    let array = str.split("/");
+    let altStr = array[array.length - 1];
+    return altStr;
+  };
 
-    return(
-        <div className="container-section">
-        <h1>{category}</h1>
-        <div>
-        {/* <img src={moviesList[0].images[0]} alt="movies"/>
-        <img src={moviesList[0].images[1]} alt="movies"/>
-        <img src={moviesList[0].images[2]} alt="movies"/> */}
-
+  return (
+    <div className="container-section">
+      <h1>{category}</h1>
+      <div>
         {images.map((image, index) => {
-           return(
-                <img key={index} src={image} alt={altMovies(image)} />
-                );
-        })};
+          return <img key={index} src={image} alt={altMovies(image)} />;
+        })}
+        ;
+      </div>
+    </div>
+  );
+};
 
-        </div>
-   
-        </div>
-
-    )
-}
-
-export default Section
+export default Section;
