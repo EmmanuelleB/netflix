684078f739195cad232e99a5f93bbcbbb1208954
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-10-07 10:40:49 +0200

    add btn next and prev

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import moviesList from "./moviesList.json";
 import Header from "./components/header/Header";
 import Section from "./components/section/Section";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+library.add(faChevronRight, faChevronLeft);
+
 function App() {
   return (
     <div>
--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -1,5 +1,6 @@
 import "./Section.css";
-import moviesList from "./../../moviesList.json";
+import { useState, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Section = ({ category, images }) => {
   const altMovies = (str) => {
@@ -8,15 +9,51 @@ const Section = ({ category, images }) => {
     return altStr;
   };
 
+  const scroll = useRef();
+
+  const slide = (shift) => {
+    scroll.current.scrollLeft += shift;
+    setscrollX(scrollX + shift);
+
+    //For checking if the scroll has ended
+    if (Math.floor(scroll.current.scrollWidth - scroll.current.scrollLeft) <= scroll.current.offsetWidth) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+  };
+
+  const scrollCheck = () => {
+    setscrollX(scroll.current.scrollLeft);
+    if (Math.floor(scroll.current.scrollWidth - scroll.current.scrollLeft) <= scroll.current.offsetWidth) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+  };
+
+  const [scrollX, setscrollX] = useState(0);
+  const [scrolEnd, setscrolEnd] = useState(false);
+
   return (
     <div className="container-section">
       <h1>{category}</h1>
-      <div>
+
+      <div className="section" ref={scroll} onScroll={scrollCheck}>
         {images.map((image, index) => {
           return <img key={index} src={image} alt={altMovies(image)} />;
         })}
-        ;
       </div>
+      {scrollX !== 0 && (
+        <button id="prev" className="prev" onClick={() => slide(-1260)}>
+          <FontAwesomeIcon icon="chevron-left" />
+        </button>
+      )}
+      {!scrolEnd && (
+        <button id="next" className="next" onClick={() => slide(+1260)}>
+          <FontAwesomeIcon icon="chevron-right" />
+        </button>
+      )}
     </div>
   );
 };
